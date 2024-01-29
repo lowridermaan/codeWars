49e1c1de4403cecf073835e3708a4a442e68195e
lowridermaan <lowridermaan@gmail.com>
2024-01-29 15:11:26 +0300

    Update howGreenIsMyValley.js

--- howGreenIsMyValley.js
+++ howGreenIsMyValley.js
@@ -79,3 +79,17 @@ function makeValley(arr) {
   }
   return result
 }
+
+
+/*
+Best solution
+
+function makeValley(arr) {
+    var leftWing = []
+    var rightWing = []
+    arr.sort((a, b) => b - a)
+      .forEach((el, i) => i % 2 == 0 ? leftWing.push(el) : rightWing.unshift(el))
+    
+    return [...leftWing, ...rightWing]
+}
+*/
